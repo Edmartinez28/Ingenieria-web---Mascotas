35d46962aeeb14d51caf68d767ea69d2b162e21a
unknown <davidmartinez28@gmail.com>
2022-11-08 12:31:46 -0500

    Avance de practica mascotas

---
+++ Pets/pets-main/index.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="sliderPrincipal">
+		<div class="cuadro">
+			<h2>Proyecto Adopción de Mascotas</h2>
+			<button>ADOPTANOS</button>
+		</div>
+		<img src="images/perro.jpg">
+	</section>
+	<section class="titulo">
+		<h2>Adopción</h2>
+	</section>
+	<section class="adopcion1">
+		<img src="images/perro2.jfif">
+		<section class="cardog">
+			<h3>Firulais</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button>Mas Información</button>
+		</section>
+	</section>
+	<section class="adopcion2">
+		<img src="images/chowchow.jfif">
+		<section class="cardog2">
+			<h3>Bingo</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. </p>
+			<button>Mas Información</button>
+		</section>	
+	</section>
+	<div class="doctores">
+		<section class="doctor1">
+			<img src="images/Doctor1.jpg">
+		</section>
+		<section class="doctor2">
+			<img src="images/Doctor2.jpg">
+		</section>
+		<section class="doctor3">
+			<img src="images/Doctor3.jpg">
+		</section>
+		<section class="doctor4">
+			<img src="images/Doctor4.jpg">
+		</section>
+	</div>
+	<section class="sponsor">
+		<img src="images/logotipoPet.png" class="logopet">
+		<img src="images/logo_blanco_utpl.png" class="logou">
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
